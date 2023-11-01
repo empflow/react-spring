940a113deed35e8ae9d6f0fa47fb3f916ce643e2
Matvey <empflow@tutanota.com>
2023-11-01 10:56:45 +0300

    shaking div animation like app icon on ios

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import { useState } from "react";
 import useMeasure from "react-use-measure";
-import { useTransition, easings, useSpringRef } from "@react-spring/web";
+import { useTransition, easings } from "@react-spring/web";
 import Square from "./Square";
 import Btn from "./Btn";
 import useGetSquaresArr from "./useGetSquaresArr";
 
-export const SQUARES_AMOUNT = 5;
+export const SQUARES_AMOUNT = 1;
 export const SQUARE_HEIGHT = 100;
 
 function App() {
   const [isSquaresVisible, setIsSquaresVisible] = useState(false);
   const squaresArray = useGetSquaresArr({ isSquaresVisible });
-  console.log(squaresArray);
-
   const [squaresContainerRef, { width }] = useMeasure();
+
   const transition = useTransition(squaresArray, {
     from: { y: "-100%", opacity: 0, height: 0, width: 0, marginTop: 0 },
     enter: { y: "0%", opacity: 1, height: SQUARE_HEIGHT, width, marginTop: 4 },
     leave: { y: "-100%", opacity: 0, height: 0, width: 0, marginTop: 0 },
     config: { duration: 500, easing: easings.easeInOutQuad },
     trail: 50,
+    keys(item) {
+      return item;
+    },
   });
 
   function toggleSquares() {
@@ -30,7 +32,7 @@ function App() {
     <main className="min-h-screen touch-none flex items-center flex-col gap-2">
       <Btn onClick={toggleSquares}>Toggle</Btn>
       <div
-        className="min-h-[100px] w-full relative max-w-[200px]"
+        className="min-h-[100px] w-full relative max-w-[100px]"
         ref={squaresContainerRef}
       >
         {transition((style, i) => {
--- src/Square.tsx
+++ src/Square.tsx
@@ -1,5 +1,12 @@
-import { AnimatedProps, animated } from "@react-spring/web";
-import { CSSProperties } from "react";
+import {
+  AnimatedProps,
+  animated,
+  config,
+  easings,
+  useSpring,
+  useSpringRef,
+} from "@react-spring/web";
+import { CSSProperties, useEffect, useState } from "react";
 import cn from "./utils/cn";
 
 interface TProps {
@@ -8,12 +15,34 @@ interface TProps {
 }
 
 function Square({ style, toggleSquares }: AnimatedProps<TProps>) {
+  const api = useSpringRef();
+  const spring = useSpring({
+    ref: api,
+    from: { rotate: -2 },
+    to: { rotate: 2 },
+    loop: {
+      reverse: true,
+    },
+    config: { easing: easings.easeInOutQuad, duration: 105 },
+  });
+
+  useEffect(() => {
+    api.start();
+    return () => {
+      api.stop();
+    };
+  }, [api, spring]);
+
   return (
-    <animated.div
-      onClick={toggleSquares}
-      className={cn("rounded m-auto bg-green-700 cursor-zoom-out")}
-      style={{ ...style }}
-    ></animated.div>
+    <animated.div className="m-auto" style={style}>
+      <animated.div
+        onClick={toggleSquares}
+        className={cn(
+          "rounded m-auto w-full h-full bg-green-700 cursor-zoom-out"
+        )}
+        style={spring}
+      ></animated.div>
+    </animated.div>
   );
 }
 
